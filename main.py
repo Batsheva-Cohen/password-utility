b75d93655f420f5d0de1a3096ba70f937b26d07a
Batsheva <bat7cn@gmail.com>
2026-06-15 22:14:54 +0300

    fix: connect main to password checker logic

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello from password-utility!")
+from creat_password.password_checker import check_password_strength
+
+
+def main() -> None:
+    password = input("Enter password: ")
+    result = check_password_strength(password)
+    print(result)
 
 
 if __name__ == "__main__":
